d5849813e4cdc9244f9e93e2a0963f389af15998
Huy Nguyen <i.love.to.smile.around@gmail.com>
2018-08-11 15:13:44 +0700

    Add existing file

--- cmd-build.bat
+++ cmd-build.bat
@@ -3,8 +3,5 @@ del /S src\obj\* /q
 del /S docs\* /q
 del api\.manifest /q
 COPY CNAME docs\CNAME
-git add .
-git commit -m "Add existing file"
-git push origin master
 docfx\docfx docfx.json --serve
 pause
